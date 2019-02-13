d48a9d7497f8d2fe58592906469a2594cac5a0fc
María Mercedes Retolaza Reyna <ret16339@uvg.edu.gt>
2019-02-12 18:42:19 -0600

    Se agrega el menu con las opciones que el usuario puede ingresar

--- main.py
+++ main.py
@@ -42,10 +42,90 @@ def glLine (x1,y1, x2,y2):
 def glFinish():
     img.writeFile("img.bmp")
 
+def menu(): 
+    os.system('cls')
+    print ('0. Salir')
+    print('1. Por renderizar una imagen negra con las siguientes líneas')
+    print('2. Por una imagen negra con las siguientes líneas (parte dos)')
+    print('3. Por renderizar por renderizar una imagen negra con las siguientes líneas (parte tres)')
+    print('4. Por renderizar por renderizar una imagen negra con las siguientes líneas (parte 4)')
+    print('5. Por renderizar un cubo')
+    print('6. Por llenar un cubo isométrico')
+
 glCreateWindow(800,400)
-glViewPort(0,0,300,300)
-glClear()
-glColor(1, 0, 0)
-glVertex(0,0)
-glLine(299,200,100,100)
-glFinish()
+
+option = True 
+while option: 
+    menu()
+    menuOption = input("Ingresa una opción del menú  >> ")
+
+    if menuOption == "1":
+        print("----")
+        input ("Has ingresado a la opción 1...\npulsa una tecla para continuar")
+        glViewPort(0,0,799,599)
+        glClear()
+        glColor(1, 0, 0)
+        glVertex(0,0)
+        glLine(299,200,100,100)
+        glFinish()
+
+    elif menuOption == "2":
+        print("----")
+        input ("Has ingresado a la opción 2...\npulsa una tecla para continuar")
+        glViewPort(0,0,799,599)
+        glClear()
+        glColor(1, 0, 0)
+        glVertex(0,0)
+       
+        glFinish()
+    
+    elif menuOption == "3":
+        print("----")
+        input ("Has ingresado a la opción 3...\npulsa una tecla para continuar")
+        glViewPort(0,0,799,599)
+        glClear()
+        glColor(1, 0, 0)
+        glVertex(0,0)
+        
+        glFinish()
+    
+    elif menuOption == "4":
+        print("----")
+        input ("Has ingresado a la opción 4...\npulsa una tecla para continuar")
+        glViewPort(0,0,799,599)
+        glClear()
+        glColor(1, 0, 0)
+        glVertex(0,0)
+       
+        glFinish()
+    
+    elif menuOption == "5":
+        print("----")
+        input ("Has ingresado a la opción 5...\npulsa una tecla para continuar")
+        glViewPort(0,0,799,599)
+        glClear()
+        glColor(1, 0, 0)
+        glVertex(0,0)
+       
+        glFinish()
+    
+    elif menuOption == "6":
+        print("----")
+        input ("Has ingresado a la opción 6...\npulsa una tecla para continuar")
+        glViewPort(0,0,799,599)
+        glClear()
+        glColor(1, 0, 0)
+        glVertex(0,0)
+       
+        glFinish()
+    
+
+    
+    
+   
+
+    elif menuOption == "0":
+        break 
+    else: 
+        print("")
+        input("No has ingresado ninguna opción correcta...\npulsa una tecla para continuar")
